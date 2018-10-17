bb288f526fc1a2127696461a7ca269368c9f89fb
Legend <jasonrfelipe@gmail.com>
2018-10-16 21:45:00 -0700

    added secret sauce

--- server.js
+++ server.js
@@ -31,11 +31,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/public"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
+console.log("SECRET MAGIC SAUCE TO MAKE THIS WORK!")
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/projectthree");
 
