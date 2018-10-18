c58dc499c3d6d20c6dffa79b894d561ec7353078
Legend <jasonrfelipe@gmail.com>
2018-10-17 20:00:57 -0700

    reverting to previous version

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 
-
-import API from "./utils/API";
-
 const App = () => (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path='/search' component={Search} />
-            <Route path='/profile' component={Login} />
-          </Switch>
-        </div>
-      </Router>
-)
+  <Router>
+    <div>
 
+      {/* <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul> */}
+
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route path='/search' component={Search} />
+        <Route path='/profile' component={Profile} />
+      </Switch>
+    </div>
+  </Router>
+
+)
 
 
 export default App;
