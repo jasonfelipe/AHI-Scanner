814253d1cb52ca238258d8801c0e3f8e8337ef58
Legend <jasonrfelipe@gmail.com>
2018-10-22 21:19:19 -0700

    reverting to working computer scanner

--- client/src/components/Scanner/ScannerSettings.js
+++ client/src/components/Scanner/ScannerSettings.js
@@ -3,49 +3,20 @@ import Quagga from 'quagga';
 import PropTypes from 'prop-types';
 
 class ScannerSettings extends React.Component {
-
+    
     render() {
         return (
-            <div id="interactive" className="viewport" />
+            <div id="interactive" className="viewport"/>
         );
     }
 
-
-
     componentDidMount() {
-        console.log(navigator.mediaDevices);
-
-
-
-
-        //testing
-        var constraints = { audio: true, video: { width: 640, height: 320 } };
-
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(function (mediaStream) {
-                var video = document.querySelector('video');
-                video.srcObject = mediaStream;
-                video.onloadedmetadata = function (e) {
-                    video.play();
-                };
-            })
-            .catch(function (err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
-
-
-
-
-
-
-
-
-
-
         Quagga.init({
             inputStream: {
-                type: "LiveStream",
+                type : "LiveStream",
                 constraints: {
-                    width: 640,
-                    height: 480,
+                    width: 320,
+                    height: 240,
                     facingMode: "environment" // or user
                 }
             },
@@ -55,14 +26,14 @@ class ScannerSettings extends React.Component {
             },
             numOfWorkers: 2,
             decoder: {
-                readers: [
-                    'upc_reader',
-                    'upc_e_reader'
+                readers : [ 
+                'upc_reader',
+                'upc_e_reader'
                 ]
-
+                
             },
             locate: true
-        }, function (err) {
+        }, function(err) {
             if (err) {
                 alert("Camera not found")
                 return console.log(err);
